f43dea43ee8652dcb78f6ce3f70bb23e37eaa325
Valenttinna <fmtinez0@gmail.com>
2023-04-28 10:41:12 -0300

    Error C3

--- Leccion02/ArgumentosVariables/src/test/TestArgumentosVariables.java
+++ Leccion02/ArgumentosVariables/src/test/TestArgumentosVariables.java
@@ -1,20 +1,14 @@
-
 package test;
-
 public class TestArgumentosVariables {
     public static void main(String[] args) {
         imprimirNuemeros(3,4,5);
         imprimirNuemeros(1,2);
-        variosParametros("Juan","Acosta", 7,8,9);
-        
-        
+        variosParametros("Juan","Acosta", 7,8,9);      
     }
-    
     private static void variosParametros(String nombre,String apellido, int ...numeros){
         System.out.println("Nombre: "+nombre);
         System.out.println("Apellido: "+apellido);
-        imprimirNuemeros(numeros);
-           
+        imprimirNuemeros(numeros);           
     }
     private static void imprimirNuemeros(int ...numeros){
        for (int i=0;i< numeros.length; i++){
